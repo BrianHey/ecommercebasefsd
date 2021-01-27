40f0a5c18ac099e32cddbc8649c90d98cdd1ee27
Brian Habib <brianhft20@gmail.com>
2021-01-27 12:36:13 -0300

    Formulario para registro de productos

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert("alo");
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+app.listen(3000);
+
+app.use("/css", express.static(__dirname + "/assets/css"));
+app.use("/js", express.static(__dirname + "/assets/js"));
+
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    layoutsDir: __dirname + "/views",
+    partialsDir: __dirname + "/views/components/",
+    defaultLayout: __dirname + "/views/layouts/main",
+  })
+);
+
+app.get("/admin/productos", (req, res) => {
+  res.render("Productos");
+});
