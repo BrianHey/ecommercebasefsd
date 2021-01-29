eee2808092558b5231e2370192744f150c9411f8
ElpolMorris <pmorales.contacto@gmail.com>
2021-01-29 16:17:32 -0300

    addEachForm

---
+++ optionsForm.js
@@ -0,0 +1,7 @@
+const optionsForm = {
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    colors: ["Azul", "Rojo", "Amarillo", "Morado", "Negro", "Blanco"],
+    genders: ["Hombre","Mujer","Unisex"]
+}
+
+module.exports = optionsForm
