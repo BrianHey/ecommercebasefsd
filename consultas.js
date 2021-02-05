c7cefccd304d6bc0a98605136eba65e892357c9b
Brian Habib <brianhft20@gmail.com>
2021-02-05 11:03:21 -0300

    ...

--- consultas.js
+++ consultas.js
@@ -2,6 +2,7 @@ const { Pool } = require("pg");
 // CREATE TABLE productos (id SERIAL PRIMARY KEY, stock INT  NOT NULL, price INT CHECK(price >=0) NOT NULL, image varchar(255) NOT NULL, category INT NOT NULL, description VARCHAR(500) NOT NULL, size VARCHAR(4) NOT NULL, gender VARCHAR(6) NOT NULL, color VARCHAR(10) NOT NULL, model VARCHAR(50), created DATESTAMP)
 // CREATE TABLE admin (id SERIAL PRIMARY KEY, username VARCHAR(50), password varchar(50));
 // CREATE TABLE transactions (id SERIAL PRIMARY KEY, order_number VARCHAR(100), date DATE, amount FLOAT, card_detail INT, payment_type VARCHAR(5)) ;
+
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
